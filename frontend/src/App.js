ad84f673bc997440c372a4e495b71d14b172a8f1
bhushan8975 <bhushansirsat300@gmail.com>
2026-04-10 21:41:26 +0530

    fix app crash

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+
+import Sidebar from "./components/Sidebar";
 import Dashboard from "./pages/Dashboard";
 import Alerts from "./pages/Alerts";
 import ThreatMap from "./pages/ThreatMap";
 import Login from "./pages/Login";
 
-function App() {
+function Layout() {
+  const location = useLocation();
+  const hideSidebar = location.pathname === "/login";
+
   return (
-    <BrowserRouter>
-      <div style={{ background: "#111", color: "#fff", minHeight: "100vh", padding: "20px" }}>
+    <div style={{ display: "flex", background: "#111", color: "#fff", minHeight: "100vh" }}>
+      {!hideSidebar && <Sidebar />}
+
+      <div style={{ flex: 1, padding: "20px" }}>
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/alerts" element={<Alerts />} />
@@ -15,8 +23,17 @@ function App() {
           <Route path="/login" element={<Login />} />
         </Routes>
       </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Layout />
     </BrowserRouter>
   );
 }
 
 export default App;
+```
