2dc14365ecb777e5a08b10de4ee88a0c09e5a5bb
bhushan8975 <bhushansirsat300@gmail.com>
2026-04-10 21:12:52 +0530

    fix blank screen final

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,52 +1,33 @@
+```javascript
+import React from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+
 import Sidebar from "./components/Sidebar";
 import Dashboard from "./pages/Dashboard";
 import Alerts from "./pages/Alerts";
 import ThreatMap from "./pages/ThreatMap";
 import Login from "./pages/Login";
-import ProtectedRoute from "./components/ProtectedRoute";
 
 function Layout() {
   const location = useLocation();
 
+  // Hide sidebar on login page
   const hideSidebar = location.pathname === "/login";
 
   return (
-    <div style={{ display: "flex", background: "#111", color: "#fff" }}>
+    <div style={{ display: "flex", background: "#111", color: "#fff", minHeight: "100vh" }}>
+      
       {!hideSidebar && <Sidebar />}
 
       <div style={{ flex: 1, padding: "20px" }}>
         <Routes>
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="/alerts"
-            element={
-              <ProtectedRoute>
-                <Alerts />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="/threat-map"
-            element={
-              <ProtectedRoute>
-                <ThreatMap />
-              </ProtectedRoute>
-            }
-          />
-
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/alerts" element={<Alerts />} />
+          <Route path="/threat-map" element={<ThreatMap />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </div>
+
     </div>
   );
 }
@@ -60,3 +41,4 @@ function App() {
 }
 
 export default App;
+```
